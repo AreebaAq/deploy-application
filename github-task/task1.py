fde4f0cc17cc5026039f1f01e0fcd901a7f9eee6
Ubuntu <ubuntu@ip-172-31-42-120.ec2.internal>
2023-12-01 09:56:01 +0000

    new folder and file created

---
+++ github-task/task1.py
@@ -0,0 +1,17 @@
+[main a90b82b] new folder and file created
+ Committer: Ubuntu <ubuntu@ip-172-31-42-120.ec2.internal>
+Your name and email address were configured automatically based
+on your username and hostname. Please check that they are accurate.
+You can suppress this message by setting them explicitly. Run the
+following command and follow the instructions in your editor to edit
+your configuration file:
+
+    git config --global --edit
+
+After doing this, you may fix the identity used for this commit with:
+
+    git commit --amend --reset-author
+
+ 2 files changed, 21 insertions(+)
+ create mode 100644 github-task/task1.sh
+ create mode 100644 github-task/uploadTask1/task1.py
--- github-task/task1.sh
+++ github-task/task1.sh
@@ -15,6 +15,6 @@ git add .
 git commit -m "new folder and file created">task1.py
 
 # Push to GitHub repository
-git remote add origin "https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_USERNAME/deploy-application.git"
+git remote set-url origin "https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_USERNAME/deploy-application.git"
 git branch -M main
 git push -u origin main
