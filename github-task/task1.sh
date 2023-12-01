a90b82be27ed7e697db7a2cb0895afc5a99f8ebc
Ubuntu <ubuntu@ip-172-31-42-120.ec2.internal>
2023-12-01 09:53:34 +0000

    new folder and file created

---
+++ github-task/task1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# CREDENTIALS
+GITHUB_USERNAME="AreebaAq"
+GITHUB_TOKEN="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"
+
+mkdir uploadTask1
+cd uploadTask1
+touch task1.py
+echo "Automate task 1">task1.py
+
+cd ..
+
+git add .
+git commit -m "new folder and file created">task1.py
+
+# Push to GitHub repository
+git remote add origin "https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_USERNAME/deploy-application.git"
+git branch -M main
+git push -u origin main
